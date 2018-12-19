ef220f935a803259a79b3de53df6ae819fd99575
nixl <nixl@cctin.com>
2018-12-19 14:35:25 +0800

    文件提交

---
+++ testSDK.framework/Headers/UsesrDefalud.h
@@ -0,0 +1,17 @@
+//
+//  UsesrDefalud.h
+//  testSDK
+//
+//  Created by 晓琳 on 2018/12/19.
+//  Copyright © 2018 晓琳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UsesrDefalud : NSObject
+-(void)testttttt;
+@end
+
+NS_ASSUME_NONNULL_END
